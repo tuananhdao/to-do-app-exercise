c8e47ebcecf38dd76bfb686b9440ddc4e277d1fd
Hanhcpp04 <hahoanganh2909@gmail.com>
2025-12-09 09:20:29 +0700

    Update SCRUM-12 Set up H2 database + Repository + Service + dtos

--- backend-java/src/main/java/com/example/backend/controller/HelloController.java
+++ backend-java/src/main/java/com/example/backend/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.backend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.config.APIResponse;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RequestMapping("/api/v1")
@@ -20,3 +23,4 @@ public class HelloController {
         return APIResponse.success("This is a demo response");
     }
 }
+    
